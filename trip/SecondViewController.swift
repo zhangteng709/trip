19770ea2ac697eba13e0f913a4bae7a3aed20696
zhengteng <zhangteng709@163.com>
2017-07-14 00:06:15 +0800

    “首次提交”

---
+++ trip/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  trip
+//
+//  Created by zhangteng709 on 17/7/13.
+//  Copyright © 2017年 zhangteng709. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
